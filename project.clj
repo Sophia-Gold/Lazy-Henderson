726a52a0800ad503c9ed361c0a10a5b49ebf5567
Sophia Gold <sophiagoldnyc@gmail.com>
2016-11-16 19:28:50 -0500

    stick man george

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject Lazy-Henderson "0.1.0-SNAPSHOT"
+(defproject Lazy-Henderson "0.1.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
